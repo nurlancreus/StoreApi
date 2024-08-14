42cd76f2f94d45e2286a514d447743b59e93f693
nurlancreus <nurlancreus007@gmail.com>
2024-08-14 17:30:43 +0400

    datetime properties nullable uptate

--- Store.Core/Entities/Base/BaseEntity.cs
+++ Store.Core/Entities/Base/BaseEntity.cs
@@ -11,6 +11,7 @@ namespace Store.Core.Entities.Base
         public Guid Id { get; set; }
         public bool IsDeleted { get; set; }
         public DateTime CreatedAt { get; set; }
-        public DateTime UpdatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+        public DateTime? DeletedAt { get; set; }
     }
 }
