14d94ca5d1ccd058d6fbb4263cfee1ae8ee08c73
nurlancreus <nurlancreus007@gmail.com>
2024-08-14 16:21:31 +0400

    adding product and category repo concretes

---
+++ Store.DAL/Repositories/Category/CategoryReadRepository.cs
@@ -0,0 +1,18 @@
+﻿using Store.Core.Abstractions.Repositories;
+using Store.Core.Entities;
+using Store.DAL.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store.DAL.Repositories
+{
+    public class CategoryReadRepository : ReadRepository<Category>, ICategoryReadRepository
+    {
+        public CategoryReadRepository(StoreApiDbContext context) : base(context)
+        {
+        }
+    }
+}
