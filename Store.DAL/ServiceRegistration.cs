5c3fc6adb549781babe0198d794167a61570e995
nurlancreus <nurlancreus007@gmail.com>
2024-08-14 16:21:46 +0400

    adding product and category repo services

--- Store.DAL/ServiceRegistration.cs
+++ Store.DAL/ServiceRegistration.cs
@@ -2,7 +2,9 @@
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.IdentityModel.Protocols;
+using Store.Core.Abstractions.Repositories;
 using Store.DAL.Context;
+using Store.DAL.Repositories;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,10 +15,18 @@ namespace Store.DAL
 {
     public static class ServiceRegistration
     {
-        public static void AddDALServices(this IServiceCollection services, IConfiguration configuration) 
+        public static void AddDALServices(this IServiceCollection services, IConfiguration configuration)
         {
-            //services.AddDbContext<MiniECommerceDbContext>(options => options.UseNpgsql(Configuration.ConnectionString));
+            // Adding db context
             services.AddDbContext<StoreApiDbContext>(options => options.UseSqlServer(configuration.GetConnectionString("Default")));
+
+            // Adding Category repositories
+            services.AddScoped<ICategoryReadRepository, CategoryReadRepository>();
+            services.AddScoped<ICategoryWriteRepository, CategoryWriteRepository>();
+            
+            // Adding Product repositories
+            services.AddScoped<IProductReadRepository, ProductReadRepository>();
+            services.AddScoped<IProductWriteRepository, ProductWriteRepository>();
         }
     }
 }
