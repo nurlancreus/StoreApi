8a3f33f6438b6c2c1c405008297f9ef33ab6f2a4
nurlancreus <nurlancreus007@gmail.com>
2024-08-19 17:52:45 +0400

    adding productimagefiles profiles

---
+++ Store.BLL/Profiles/ProductImageFilesProfile.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using Store.Core.Entities;
+using Store.DTO.DTOs.Product;
+using Store.DTO.DTOs.ProductImageFile;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store.BLL.Profiles
+{
+    public class ProductImageFilesProfile : Profile
+    {
+        public ProductImageFilesProfile()
+        {
+            CreateMap<ProductImageFile, ProductImageFilePostDTO>().ReverseMap();
+            CreateMap<ProductImageFile, ProductImageFileGetDTO>().ReverseMap();
+        }
+    }
+}
