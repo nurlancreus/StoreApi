bc1625b27a3f5bf888c0b0b2b48b884b975cf38a
nurlancreus <nurlancreus007@gmail.com>
2024-08-21 18:12:31 +0400

    adding productimagefile repos and IStorage

--- Store.Core/Abstractions/Services/Storage/IStorage.cs
+++ Store.Core/Abstractions/Services/Storage/IStorage.cs
@@ -11,8 +11,10 @@ namespace Store.Core.Abstractions.Services.Storage
     {
         Task<List<(string fileName, string path)>> UploadAsync(string path, IFormFileCollection formFiles);
         Task DeleteAsync(string path, string fileName);
+        Task DeleteByUrlAsync(string url);
         Task DeleteAllAsync(string path);
         Task<List<string>> GetFilesAsync(string path);
         Task<bool> HasFileAsync(string path, string fileName);
+        Task<string> GetUploadedFileUrlAsync(string path, string fileName);
     }
 }
