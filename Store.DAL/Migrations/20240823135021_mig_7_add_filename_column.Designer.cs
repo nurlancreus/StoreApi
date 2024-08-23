5962d9a1642b741b8a3f438bf595b2afdbad11aa
nurlancreus <nurlancreus007@gmail.com>
2024-08-23 18:04:44 +0400

    adding new migrations

--- Store.DAL/Migrations/20240823135021_mig_7_add_filename_column.Designer.cs
+++ Store.DAL/Migrations/20240823135021_mig_7_add_filename_column.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Store.DAL.Context;
 
@@ -11,9 +12,11 @@ using Store.DAL.Context;
 namespace Store.DAL.Migrations
 {
     [DbContext(typeof(StoreApiDbContext))]
-    partial class StoreApiDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240823135021_mig_7_add_filename_column")]
+    partial class mig_7_add_filename_column
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Store.DAL/Migrations/20240823135021_mig_7_add_filename_column.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Store.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class mig_7_add_filename_column : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UpdatedAt",
+                table: "AppFiles");
+
+            migrationBuilder.AddColumn<string>(
+                name: "FileName",
+                table: "AppFiles",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FileName",
+                table: "AppFiles");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedAt",
+                table: "AppFiles",
+                type: "datetime2",
+                nullable: true);
+        }
+    }
+}
